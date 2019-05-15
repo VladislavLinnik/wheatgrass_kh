854dcc0570b22f61a851dce157251f9297cdc6de
Vlad Linnik <vladislav.linnik.1999@gmail.com>
2019-05-15 16:42:08 +0300

    added some blocks

--- app/js/index.js
+++ app/js/index.js
@@ -2,10 +2,29 @@ $(document).ready(function(){
     
     // popup for contact
     $('.menu-toggle').on('click', function(){
-        $('body').toggleClass('open');
+        $('body').addClass('open');
     });
     $('.menu-toggle-close').on('click', function(){
-        $('body').toggleClass('open');
+        $('body').removeClass('open');
+    });
+
+    // popup for nav menu
+    $('.menu-nav-toggle').on('click', function(){
+        $('body').addClass('open-nav');
+    });
+    $('.menu-nav-toggle-close').on('click', function(){
+        $('body').removeClass('open-nav');
+    });
+
+    $('.popup-nav').on('click', 'a', function(event){
+        event.preventDefault();
+        
+        $('body').removeClass('open-nav');
+
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        
+        $('body,html').animate({scrollTop: top}, 500);
     });
     
     // Smooth transition by anchors
@@ -21,6 +40,16 @@ $(document).ready(function(){
         
         //анимируем переход на расстояние - top за 500 мс
         $('body,html').animate({scrollTop: top}, 500);
+    });
+
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100){
+            $('.menu-nav-toggle').addClass("active");
+        }
+        else{
+            $('.menu-nav-toggle').removeClass("active");
+        }
     });
 
 });
